5177b49a493ea12cad99ac31e21bb30a8a26fbb6
LENOVO <LENOVO@CHIVL>
2024-12-23 08:35:53 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace peringatan_notif
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Toast toast = new Toast();
+            toast.Show();
+
+        }
+    }
+}
